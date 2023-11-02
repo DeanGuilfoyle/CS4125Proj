e13db960139ae18520c6829196bb2fa5b2b03ff7
[deanguilfoyle] <[20182062@studentmail.ul.ie]>
2023-11-02 19:05:15 +0000

    Added add car functionality, some basic styling

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,3 +1,9 @@
+from django import forms
 from django.contrib import admin
+from .models import Car
 
-# Register your models here.
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('make', 'model', 'year', 'price_per_day', 'is_available')
+
+    form = forms.ModelForm
--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .views import CarListView
 
 urlpatterns = [
     #path("", views.index, name="index"),
     #path("create/", views.create, name="create"),
     path("", views.home, name="home"),
+    path('car-list/', CarListView.as_view(), name='car-list'),
 ]
